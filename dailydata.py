e87bb5f29e96d2ca785affc57ed82962344de2c2
Emre <43857443+EmreErbek@users.noreply.github.com>
2022-07-11 18:32:28 +0300

    Update dailydata.py

--- dailydata.py
+++ dailydata.py
@@ -4,7 +4,7 @@ import pandas as pd
 #for Turkey .IS on yfinance
 hisse = input("key")
 dailydata = yf.Ticker(hisse+".IS)
-history = dailydata.history(period="max")
+history = pd.DataFrame(dailydata.history(period="max"))
 
 with pd.ExcelWriter("{}.xlsx".format(hisse),mode="a") as writer:
   history.to_excel(writer, sheet_name="Technical Daily Data")
